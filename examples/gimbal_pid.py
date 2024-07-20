ba663a5f797f632b7daa6a41197b7c988c5e1742
unnxp <pram181147@gmail.com>
2024-07-20 12:24:08 +0700

    pid

--- examples/gimbal_pid.py
+++ examples/gimbal_pid.py
@@ -71,13 +71,11 @@ if __name__ == "__main__":
     i=1
     # the image center constants4
     
-    center_x = (1280 / 4)
+    center_x = (1280 / 2)
     center_y = 720 / 2
     
 
     ep_camera.start_video_stream(display=False)
-    end = time.time()
-    print(end-start)
     ep_gimbal.sub_angle(freq=50, callback=sub_data_handler)
     result = ep_vision.sub_detect_info(name="marker", callback=on_detect_marker)
 
@@ -88,23 +86,25 @@ if __name__ == "__main__":
     time.sleep(1)
 
     # PID controller constants
-    p = 0.33
+    p = 0.83/1.7
+    i= p/(0.6/2)
+    d = p*(0.6/8)
+    # i = 0
+    # d = 0
 
     data_pith_yaw = []
-
+    accumulate_x = 0
+    accumulate_y = 0
     # loop การทำงานของหุ่น
-
+    prev_time = time.time()
+    prev_err_x = 0
+    prev_err_y = 0
     while True:
-    
-        if count >= 200:
-            center_x = (1280 / 4)*3
-        if count == 400:
-            count = 0
-            center_x = (1280 / 4)
-        print(count)
         if len(markers) != 0:  # target found
             after_time = time.time()
             x, y = markers[-1].center  # x,y here in the pixel unit
+            add_time = time.time() - start
+            
 
             err_x = (
                 center_x - x
@@ -112,29 +112,44 @@ if __name__ == "__main__":
             err_y = (
                 center_y - y
             )  # err_y = image_center in y direction - current marker center in y direction
-            if after_time==3:
-                center_x *=1/2
-            if after_time==6:
-                center_x*=3
+            accumulate_x += err_x*(after_time-prev_time)
+            accumulate_y += err_y*(after_time-prev_time)
+
             if count >= 1:
                 # คำนวณความเร็วในการหมุน gimbal โดยใช้ PID
                 speed_x = (
-                    (p * err_x)
+                    (p * err_x)+ d*((err_x-prev_err_x)/(after_time-prev_time)) + i*accumulate_x
                 )
                 speed_y = (
-                    (p * err_y)
+                    (p * err_y)+ d*((err_y-prev_err_y)/(after_time-prev_time)) + i*accumulate_y
                 )
 
                 # หมุน gimbal ตามความเร็วที่คำนวณมาก
                 ep_gimbal.drive_speed(pitch_speed=speed_y, yaw_speed=-speed_x)
+                time.sleep(0.001)
             
                 # เก็บค่ามุมของ gimbal, error x, error y, speed x, speed y
+                print(add_time)
                 data_pith_yaw.append(
                     list(list_of_data)
-                    + [err_x, err_y, round(speed_x, 3), round(speed_y, 3), center_x, x]
+                    + [err_x, err_y, round(speed_x, 3), round(speed_y, 3), center_x, x,add_time]
                 )
-
+            
             count += 1
+            
+
+            
+            # prev_time = endtimmme
+            # # delt_err_x = err_x-prev_err_x
+            # prev_err_x = err_x
+            # # delt_err_y = err_y-prev_err_x
+            # prev_err_y = err_y
+            print('prev_time :',prev_time,'err_x',err_x,'abs(err_x-prev_err_x)',abs(err_x-prev_err_x),'(abs(err_y-prev_err_y)',abs(err_y-prev_err_y))
+            prev_time = after_time
+            # delt_err_x = err_x-prev_err_x
+            prev_err_x = err_x
+            # delt_err_y = err_y-prev_err_x
+            prev_err_y = err_y
 
         else:
             # หมุนกลับ center
@@ -176,12 +191,13 @@ if __name__ == "__main__":
     y_point7 = [i[7] for i in data_pith_yaw]
     y_point8 = [i[8] for i in data_pith_yaw]
     y_point9 = [i[9] for i in data_pith_yaw]
+    y_point10 = [i[10] for i in data_pith_yaw]
     '''y_t = 
     x_t = [1280/2,1280/4,(1280/4)*3]'''
 
 
-    plt.plot(x_point, y_point8)
-    plt.plot(x_point, y_point9)
+    plt.plot(y_point10, y_point8)
+    plt.plot(y_point10, y_point9)
     # plt.plot(x_point, y_point4)
     # plt.plot(x_point, y_point5)
     # plt.plot(x_point, y_point6)
