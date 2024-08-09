5009d1537ee3cf8e5b1122928f8c30b79e290878
NUMNIMx <itspeemgmth@gmail.com>
2024-08-09 21:10:45 +0700

    new file add :)

---
+++ code_explorer/wave_move_forward.py
@@ -0,0 +1,10 @@
+def move_forward(l_tof,axis,s):
+    lst_c_pos = {'x_c':[],'y_c':[]}
+    if s[1] == 0:
+        ep_chassis.drive_wheels(w1=speed, w2=speed, w3=speed, w4=speed)
+        print(l_tof[-1])
+
+        if l_tof[-1]<=310:
+            ep_chassis.drive_wheels(w1=0, w2=0, w3=0, w4=0)
+            lst_c_pos['x_c'].append(axis['x'][-1]);  lst_c_pos['y_c'].append(axis['y'][-1])
+            print(lst_c_pos)
