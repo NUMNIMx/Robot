58c5fdc331722d140976867443f9556a4dd02763
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-08-29 19:35:02 +0700

    adding th and color in HSV

---
+++ lab_4_immage/lab5.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+#เทสรูป
+image = r'E:\Users\Home\Desktop\New folder (12)\Robot\lab_4_immage\coke.webp'
+r_image = cv2.imread(image)
+#เปลี่ยนเป็นHSL
+HSL_image = cv2.cvtColor(r_image,cv2.COLOR_BGR2HSV)
+#เเบ่งสีค่าHSV 3 ค่า [:,:,:,]ค่าเเรกคือ องศ่่าในรูป 2กับคือคือเปแเสนความสว่างจากขาวไปดำ  3คือ ความเข้ม ของสี
+lower_red = np.array([0, 100, 100])
+upper_red = np.array([10, 255, 255])
+#inrage คือฟังชั้นเทรชโอส ก็คือง่าย if elif กำหนดค่า 
+mask = cv2.inRange(HSL_image,lower_red,upper_red)
+cv2.imshow('image test', mask)
+cv2.waitKey(0)
