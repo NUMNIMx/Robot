23835b4ca8ff499c152afe19801f69678ae8a72a
thanathat26 <tphone0935@gmail.com>
2024-09-10 20:07:25 +0700

    kukkaicut

---
+++ Assigntment_02/ROI.py
@@ -0,0 +1,15 @@
+import cv2
+
+# อ่านภาพ
+image = cv2.imread('/Users/seaconcoding/Desktop/robottest/Robot/lab_4_immage/a5_0floor.jpg')
+
+# เลือก ROI
+roi = cv2.selectROI("Select ROI", image, fromCenter=False, showCrosshair=True)
+
+# ตัดภาพเฉพาะส่วนที่สนใจ
+roi_cropped = image[int(roi[1]):int(roi[1] + roi[3]), int(roi[0]):int(roi[0] + roi[2])]
+cv2.imwrite('0.jpg', roi_cropped)
+# แสดงภาพที่ตัดออกมา
+cv2.imshow("Cropped ROI", roi_cropped)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
